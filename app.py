bb8d7569180f1452b273c0090ac7dee992999e17
Abely Messiah <codewithabel1@gmail.com>
2025-07-03 14:25:02 +0300

    update

--- app.py
+++ app.py
@@ -19,39 +19,72 @@ def predict():
     if request.method == 'POST':
         try:
             #Toa data kutoka kwenye form
-            features = [
-                float(request.form['GDP_per_Capita']),
-                float(request.form['Social_Support']),
-                float(request.form['Healthy_Life_Expectancy']),
-                float(request.form['Freedom']),
-                float(request.form['Generosity']),
-                float(request.form['Corruption_Perception']),
-                float(request.form['Unemployment_Rate']),
-                float(request.form['Education_Index']),
-                float(request.form['Population']),
-                float(request.form['Urbanization_Rate']),
-                float(request.form['Public_Trust']),
-                float(request.form['Mental_Health_Index']),
-                float(request.form['Income_Inequality']),
-                float(request.form['Public_Health_Expenditure']),
-                float(request.form['Climate_Index']),
-                float(request.form['Work_Life_Balance']),
-                float(request.form['Internet_Access']),
-                float(request.form['Crime_Rate']),
-                float(request.form['Political_Stability']),
-                float(request.form['Employment_Rate']),
-                float(request.form['Life_Satisfaction'])
-            ]
+            
+            gdp = float(request.form['GDP_per_Capita'])
+            Social_Support = float(request.form['Social_Support'])
+            Healthy_Life_Expectancy = float(request.form['Healthy_Life_Expectancy'])
+            Freedom = float(request.form['Freedom'])
+            Generosity = float(request.form['Generosity'])
+            Corruption_Perception = float(request.form['Corruption_Perception'])
+            Unemployment_Rate = float(request.form['Unemployment_Rate'])
+            Education_Index = float(request.form['Education_Index'])
+            Population = float(request.form['Population'])
+            Urbanization_Rate = float(request.form['Urbanization_Rate'])
+            Public_Trust = float(request.form['Public_Trust'])
+            Mental_Health_Index = float(request.form['Mental_Health_Index'])
+            Income_Inequality = float(request.form['Income_Inequality'])
+            Public_Health_Expenditure = float(request.form['Public_Health_Expenditure'])
+            Climate_Index = float(request.form['Climate_Index'])
+            Work_Life_Balance = float(request.form['Work_Life_Balance'])
+            Internet_Access = float(request.form['Internet_Access'])
+            Crime_Rate = float(request.form['Crime_Rate'])
+            Political_Stability = float(request.form['Political_Stability'])
+            Employment_Rate = float(request.form['Employment_Rate'])
+            Life_Satisfaction = float(request.form['Life_Satisfaction'])
 
-            #Badilisha kuwa numpy na fanya prediction
+            features = [[gdp,Social_Support,  Healthy_Life_Expectancy, Freedom, Generosity, Corruption_Perception, Unemployment_Rate, Education_Index, Population, Urbanization_Rate, Public_Trust, Mental_Health_Index, Income_Inequality, Public_Health_Expenditure, Climate_Index, Work_Life_Balance, Internet_Access, Crime_Rate, Political_Stability, Employment_Rate, Life_Satisfaction]]
 
-            final_features = np.array([features])
-            prediction = model.predict(final_features)[0]
+    
+            prediction = model.predict(features)[0]
 
-            return render_template('index.html',
-                                    prediction_text=f"Happiness Score InakadiriwaP: {prediction:.2f}")
+            if prediction >= 6:
+                interpretation = "Full Peace/ Safe"
+
+            elif prediction >= 4:
+                interpretation = "Simple Peace"
+
+            else:
+                interpretation = "No more  Peace"
+
+            return render_template('results.html',
+                                    prediction=round(prediction, 2),
+                                    interpretation=interpretation,
+                                    inputs={
+                                        "GDP per Capita": gdp,
+                                        "Freedom": Freedom,
+                                        "Social_Support": Social_Support, 
+                                        "Healthy_Life_Expectancy": Healthy_Life_Expectancy, 
+                                        "Generosity ": Generosity ,
+                                        "Corruption_Perception" : Corruption_Perception, 
+                                        "Unemployment_Rate ": Unemployment_Rate ,
+                                        "Education_Index " :Education_Index ,
+                                        "Population ": Population ,
+                                        "Urbanization_Rate" : Urbanization_Rate,
+                                        "Public_Trust " : Public_Trust ,
+                                        "Mental_Health_Index" : Mental_Health_Index,
+                                        "Income_Inequality " : Income_Inequality ,
+                                        "Public_Health_Expenditure " : Public_Health_Expenditure ,
+                                        "Climate_Index " : Climate_Index ,
+                                        "Work_Life_Balance ": Work_Life_Balance ,
+                                        "Internet_Access " : Internet_Access ,
+                                        "Crime_Rate " : Crime_Rate ,
+                                        "Political_Stability" : Political_Stability, 
+                                        "Employment_Rate " : Employment_Rate ,
+                                        "Life_Satisfaction " : Life_Satisfaction 
+
+                                    })
         except Exception as e:
-            return render_template('index.html', prediction_text=f"Error: {e}")
+            return  f"Error: {e}"
         
 if __name__ == '__main__':
     app.run(debug=True)
